283285e53b8d73822eb46a9f85581fd9a36fb136
sebastiangolab <sebagolab97@gmail.com>
2022-01-31 23:07:21 +0100

    Remove sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,8 @@ module.exports = {
     description: `My name is Wioletta and I am a professional and qualified hair stylist with many years of experience and the love for creating beautiful hair is my passion.`,
     author: `@sebastiangolab`,
     keywords: `stylist, hairdresser`,
-    siteUrl: `https://wiolettagruszecka.co.uk`,
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp",  "gatsby-plugin-fontawesome-css", "gatsby-plugin-root-import",
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp",  "gatsby-plugin-fontawesome-css", "gatsby-plugin-root-import",
   {
     resolve: `gatsby-source-datocms`,
     options: {
